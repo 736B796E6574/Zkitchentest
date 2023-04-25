b35a112fde812d4e1532fb1c4cde2c2ac7f065d2
Martin McInerney <martin_mcinerney@live.ie>
2023-04-25 14:16:08 +0000

    fixed allowed host

--- Restaurant_booking/settings.py
+++ Restaurant_booking/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cookhouse.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['kitchentesth.herokuapp.com', 'localhost']
 
 
 # Application definition
