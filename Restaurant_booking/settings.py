32957c32c0956d81dede25e22de31c3c292fe070
Awsomenesss <>
2023-04-25 12:11:09 +0000

    add domin

--- Restaurant_booking/settings.py
+++ Restaurant_booking/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ZKITCHEN.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ZKITCHEN.herokuapp.com',
+                 'localhost', 'https://cookhouse.herokuapp.com/']
 
 
 # Application definition
